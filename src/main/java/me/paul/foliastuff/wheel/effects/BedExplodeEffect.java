7272952315e5a58b42e826e2063c31e7db6d856e
svyoshi <gn.josephp@gmail.com>
2024-07-24 13:43:40 -0500

    Re-enable stone tools Changed Bed Explode to independenet effect Added 2 teleport effects

--- src/main/java/me/paul/foliastuff/wheel/effects/BedExplodeEffect.java
+++ src/main/java/me/paul/foliastuff/wheel/effects/BedExplodeEffect.java
@@ -1,53 +1,36 @@
-//package me.paul.foliastuff.wheel.effects;
-//package me.paul.lads.wheel.effects;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import org.bukkit.Bukkit;
-//import org.bukkit.ChatColor;
-//import org.bukkit.Location;
-//import org.bukkit.Sound;
-//import org.bukkit.entity.Player;
-//import org.bukkit.entity.TNTPrimed;
-//
-//import me.paul.lads.util.scheduler.Sync;
-//import me.paul.lads.wheel.GenerateEffect;
-//import me.paul.lads.wheel.Wheel;
-//import me.paul.lads.wheel.WheelEffect;
-//
-//@GenerateEffect(description = "Explodes everyones beds", key = "effect_bed", name = "Explode Beds")
-//public class BedExplodeEffect extends WheelEffect {
-//	String prefix = ChatColor.GRAY + ChatColor.ITALIC.toString() + "[The Wacky" + ChatColor.RED + ChatColor.ITALIC
-//			+ " WHEEL" + ChatColor.GRAY + ChatColor.ITALIC + "] -> me: ";
-//
-//	@Override
-//	public void play(Player spinner, Wheel spun) {
-//
-//		Bukkit.getOnlinePlayers().forEach(p -> p.sendTitle(ChatColor.DARK_RED + "Wouldn't wanna sleep in your bed.",
-//				"There's a suprise at home.", 10, 20 * 5, 10));
-//		Bukkit.getOnlinePlayers().forEach(p -> p.playSound(p.getLocation(), Sound.ENTITY_BLAZE_DEATH, 1f, 1f));
-//		Bukkit.broadcastMessage(ChatColor.GREEN + spinner.getName() + " left you a present at home.");
-//
-//		List<Location> locations = new ArrayList<>();
-//		for (Player player : Bukkit.getOnlinePlayers()) {
-//			try {
-//				Location bedLoc = player.getBedSpawnLocation();
-//				if (bedLoc == null) {
-//					player.teleport(player.getLocation().clone().add(0, 50, 0));
-//					player.sendMessage(prefix + "Oh you think you're safe because you don't have a bed? Wrong. Up you go!");
-//					continue;
-//				} else {
-//					locations.add(bedLoc);
-//				}
-//			} catch (Exception e) {
-//			}
-//		}
-//		Sync.get().cycles(5).delay(40).run(() -> {
-//			for (Location l : locations) {
-//				Location bedLoc = l.clone().add(0, 5, 0);
-//				bedLoc.getWorld().spawn(bedLoc, TNTPrimed.class);
-//			}
-//		});
-//	}
-//}
+package me.paul.foliastuff.wheel.effects;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
+
+import me.paul.foliastuff.wheel.GenerateEffect;
+import me.paul.foliastuff.wheel.Wheel;
+import me.paul.foliastuff.wheel.WheelEffect;
+
+@GenerateEffect(description = "Explodes your bed", key = "effect_bed", name = "Explode Bed")
+public class BedExplodeEffect extends WheelEffect {
+	String prefix = ChatColor.GRAY + ChatColor.ITALIC.toString() + "[The Wacky" + ChatColor.RED + ChatColor.ITALIC
+			+ " WHEEL" + ChatColor.GRAY + ChatColor.ITALIC + "] -> me: ";
+
+	@Override
+	public void play(Player spinner, Wheel spun) {
+
+    spinner.playSound(spinner.getLocation(), Sound.ENTITY_BAT_DEATH, 1f, 1f);
+		spinner.sendTitle(ChatColor.DARK_RED + "Wouldn't wanna sleep in your bed.",
+				"There's a surprise at home.", 10, 20 * 5, 10);
+
+    Location loc = spinner.getRespawnLocation();
+
+    if (loc != null) {
+      loc.getWorld().spawn(loc, TNTPrimed.class);
+      Bukkit.broadcastMessage(ChatColor.GREEN + spinner.getName() + " just had their bed exploded.");
+    } else {
+      spinner.teleport(spinner.getLocation().clone().add(0, 50, 0));
+      spinner.sendMessage(prefix + "Oh, you think you're safe because you don't have a bed? Wrong. Up you go!");
+    }
+	}
+}
